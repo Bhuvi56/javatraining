c62b6d91f7b4acd676bbb8c4427ba90498b74b6a
Bhuvaneshwari.D <Bhuvaneshwari.D@EXIND3020-53.allfon.in>
2022-07-06 12:42:39 +0530

    boot

---
+++ Spring-boot_actuator/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class BookController {
+	
+	
+	@GetMapping(path = "/books")
+	public List<String>getBooks(){
+		log.info("get  books called");
+		return Arrays.asList("java","spring in action","c++");
+	}
+
+	
+	@GetMapping(path="/books/{id}")
+	public String getBookById(@PathVariable int id) {
+		
+		if(id==1) {
+			return "java";
+		}else {
+		    return	"spring in action";
+		}
+	}
+}
