0e4c4de057cb758d714a966be8c9bc00b6d6a968
Bhuvaneshwari.D <Bhuvaneshwari.D@EXIND3020-53.allfon.in>
2022-07-06 12:44:42 +0530

    micro

---
+++ microservice-two/src/main/java/com/example/demo/model/DriverDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DriverDTO {
+	
+	int driverId;
+	String driverName;
+	long mobileNumber;
+	double rating;
+
+}
