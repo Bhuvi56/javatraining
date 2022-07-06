66abb7a905645ac66cfcfebfdcc0ad3879894e2e
Bhuvaneshwari.D <Bhuvaneshwari.D@EXIND3020-53.allfon.in>
2022-07-06 12:31:26 +0530

    security

---
+++ Spring-security/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+	
+	
+	
+	@Autowired
+	BCryptPasswordEncoder encoder;
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	String sql="SELECT u.username, a.authority FROM bhu_authorities a, bhu_users u  WHERE u.username = AND u.username = a.username"; 
+			
+//	auth.inMemoryAuthentication().withUser("india").password(encoder.encode("india")).roles("ADMIN")
+//	.and().withUser("nepal").password(encoder.encode("nepal")).roles("GUEST");	
+//		
+		
+		auth.jdbcAuthentication().dataSource(dataSource).
+		usersByUsernameQuery("select username,password,enabled from bhu_users where username=?")
+		.authoritiesByUsernameQuery(sql).passwordEncoder(encoder);
+	
+			
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeHttpRequests().antMatchers("/api/v1/**").authenticated().and().httpBasic();
+		
+	}
+
+}
